d96953ff775ddc2e292dc53bfe8c58614f0b5847
geuzkp <zkp1985@gmail.com>
2019-05-08 00:39:09 +0800

    xxx

---
+++ Assets/Script/CheckCollison.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollison : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+    }
+    */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject lgameObject = collision.gameObject;
+        lgameObject.SetActive(false);
+        Rigidbody2D rd = collision.GetComponent<Rigidbody2D>();
+        rd.transform.position = new Vector2(-1.55f, -9f);
+
+        GameController.instance.updateScore(1);
+        
+    }
+
+
+}
---
+++ Assets/Script/ScrollObjectCoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObjectCoin: MonoBehaviour
+{
+
+    private Rigidbody2D rd2d;
+
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        rd2d.velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rd2d.velocity = GameController.instance.GetPlayerSpeed(); 
+    }
+}
